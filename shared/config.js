717fea99cba99867d9860a0c5c9a2980eb92eb15
A2-0I <arwoodraecmwellli@gmail.com>
2026-09-23 16:19:49 +0900

    Update Supabase URL and anon key in config.js

--- shared/config.js
+++ shared/config.js
@@ -2,5 +2,5 @@
 // Project Settings > API 에서 Project URL / anon key 확인
 // URL에는 /rest/v1 을 붙이지 마세요.
 
-window.AI_PROCESSOR_SUPABASE_URL = 'https://vlveepihxzsqderejtbc.supabase.co';
-window.AI_PROCESSOR_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsdmVlcGloeHpzcWRlcmVqdGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5MjMyOTEsImV4cCI6MjEwNDQ5OTI5MX0.rN2FOJIhKFcjafbqV72pudjOtsocVxtCaV_c8l4WHmg';
+window.AI_PROCESSOR_SUPABASE_URL = 'https://czlomfplhegwrmvsknhi.supabase.co';
+window.AI_PROCESSOR_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6bG9tZnBsaGVnd3JtdnNrbmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxMzk0MzcsImV4cCI6MjEwNTcxNTQzN30.j-BtmQ5dUDrelw23y1ltMVzQdSUarbvMkQwadTszbd8';
